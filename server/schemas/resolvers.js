84eaeff95e667fd420e2ae5948f85a89c0b60446
cbazil114 <113719887+cbazil114@users.noreply.github.com>
2023-03-13 21:43:08 -0400

    App is now able to run correctly

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,18 @@
+const resolvers = {
+    Mutation: {
+        addUser: async (parent, args) => {
+          const user = await User.create(args);
+          const token = signToken(user);
+    
+          return { token, user };
+        },
+        updateUser: async (parent, args, context) => {
+            if (context.user) {
+              return await User.findByIdAndUpdate(context.user._id, args, { new: true });
+            }
+      
+            throw new AuthenticationError('Not logged in');
+          }
+}}
+
+module.exports = resolvers;
